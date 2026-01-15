8844966eea4e0447fa7d95c8dc29040af19e4d70
AlejandroBR <barrionuevorosadoalejandro@gmail.com>
2026-01-15 20:22:00 +0100

    feat: finishing the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,40 @@ employes_df
 
 st.divider()
 
-with st.container(horizontal=True):
+col1, col2, col3 = st.columns(3)
+
+with col1:
     color = st.color_picker("Elige un color para las barras", "#3475B3")
+
+with col2:
     names = st.toggle("Mostrar el nombre")
-    salary = st.toggle("Mostrar sueldo en la barra")
+
+with col3:
+    salarys = st.toggle("Mostrar sueldo en la barra")
+
+
+y_pos = range(len(employes_df))
+
+fig, ax = plt.subplots()
+
+bars = ax.barh(
+    y_pos,
+    employes_df["salary"],
+    color=color
+)
+
+if salarys:
+    for i, salary in enumerate(employes_df["salary"]):
+        ax.text( salary + 20, i, f"{salary} €", va="center", color="black")
+
+if names:
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(employes_df["full name"])
+else:
+    ax.set_yticks([])
+
+ax.set_xlim(0, 4500)
+
+st.pyplot(fig)
+
+st.write("© Alejandro Barrionuevo Rosado - CPIFP Alan Turing")
